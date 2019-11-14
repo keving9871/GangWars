b1158fff8253aa41389ab7d9d6ccdbe84d75f6aa
Kevin Garcia <kjg365@nyu.edu>
2019-11-14 15:03:46 -0500

    Made a Territory Controller

---
+++ Gang Wars/Assets/Scripts/TerritoryController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerritoryController : MonoBehaviour
+{
+    //Variables:
+    public float territoryProgress;
+    public bool gangOwned;
+
+    void Start()
+    {
+        territoryProgress = 0f;
+        gangOwned = false;
+    }
+
+    void Update()
+    {
+        if (territoryProgress >= 5000f)
+        {
+            gangOwned = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (territoryProgress >= 0)
+        territoryProgress -= 1f;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            territoryProgress += 5f;
+        }
+    }
+}
